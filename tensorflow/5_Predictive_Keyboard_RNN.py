e85d928dcf9588ec9e7990dca51bb4f5c617fea7
Mortafix <moris.doratiotto@gmail.com>
2018-04-22 20:06:52 +0200

    First example of Recursive NN for a predictive keyboard

---
+++ tensorflow/5_Predictive_Keyboard_RNN.py
@@ -0,0 +1,132 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation
+from keras.layers import LSTM, Dropout
+from keras.layers import TimeDistributed
+from keras.layers.core import Dense, Activation, Dropout, RepeatVector
+from keras.optimizers import RMSprop
+import matplotlib.pyplot as plt
+import pickle
+import sys
+import heapq
+import seaborn as sns
+from pylab import rcParams
+
+# Settings
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+rcParams['figure.figsize'] = 12, 5
+np.random.seed(42)
+tf.set_random_seed(42)
+
+# Text Settings
+path = 'data/books/Friedrich_Nietzsche_Beyond_Good_and_Evil.txt'
+text = open(path).read().lower()
+print('Corpus length:',len(text))
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+print("Unique chars:",len(chars))
+
+# Preparing train set  
+SEQUENCE_LENGTH = 40
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - SEQUENCE_LENGTH, step):
+    sentences.append(text[i: i + SEQUENCE_LENGTH])
+    next_chars.append(text[i + SEQUENCE_LENGTH])
+print("Training examples:", len(sentences))
+
+# Preapring test set (for next char in chunks)
+X = np.zeros((len(sentences), SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+# Building the model
+model = Sequential()
+model.add(LSTM(128, input_shape=(SEQUENCE_LENGTH, len(chars))))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+optimizer = RMSprop(lr=0.01)
+#model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+#history = model.fit(X, y, validation_split=0.05, batch_size=128, epochs=20, shuffle=True).history
+
+# Saving progress (slow training)
+#model.save('keras_model.h5')
+#pickle.dump(history, open("lp_history.p", "wb"))
+model = load_model('dump/books/keras_model.h5')
+history = pickle.load(open("dump/books/fn_history.p", "rb"))
+
+# Evalution
+plt.plot(history['acc'])
+plt.plot(history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+plt.plot(history['loss'])
+plt.plot(history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+
+# PREDICTION MODEL
+# Prepare the text
+def prepare_input(text):
+    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+    for t, char in enumerate(text):
+        x[0, t, char_indices[char]] = 1.
+    return x
+
+# Predict the next N most probable words
+def sample(preds, top_n=3): 
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+
+# Predict the text completion (until the space)
+def predict_completion(text):
+    original_text = text
+    generated = text
+    completion = ''
+    while True:
+        x = prepare_input(text)
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, top_n=1)[0]
+        next_char = indices_char[next_index]
+        text = text[1:] + next_char
+        completion += next_char  
+        if len(original_text + completion) + 2 > len(original_text) and next_char == ' ':
+            return completion
+
+# Predict multiple text completions
+def predict_completions(text, n=3):
+    x = prepare_input(text)
+    preds = model.predict(x, verbose=0)[0]
+    next_indices = sample(preds, n)
+    return [indices_char[idx] + predict_completion(text[1:] + indices_char[idx]) for idx in next_indices]
+
+# Examples
+quotes = [
+    "It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
+    "That which does not kill us makes us stronger.",
+    "I'm not upset that you lied to me, I'm upset that from now on I can't believe you.",
+    "And those who were seen dancing were thought to be insane by those who could not hear the music.",
+    "It is hard enough to remember my opinions, without also remembering my reasons for them!"
+]
+for q in quotes:
+    seq = q[:40].lower()
+    print(seq)
+    print(predict_completions(seq, 5))
+    print()
